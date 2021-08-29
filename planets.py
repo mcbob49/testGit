1741a6a1027d96a4ebc9ae2b155c0ff65128e852
mcbob49 <mybob492000@yahoo.com>
2021-08-29 10:38:42 -0700

    initial commit

---
+++ planets.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.6
+#parses the planets.csv file
+import os
+import csv
+
+filename = "/home/bob/scripts/textfiles/planets.txt"
+
+with open(filename,'r') as csv_file:
+	readContents= csv_file.readlines()
+	#print(readContents)
+	for line in readContents:
+		print(line,end='')
+		print(line.split(", - "))
+	
